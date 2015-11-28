87cdc1e40bd36b8685c3f01fcd37a957691fd0f6
TRON <osvaldocube@gmail.com>
2015-11-27 20:58:40 -0600

    Commit Inicial

--- Phone Register/Register.m
+++ Phone Register/Register.m
@@ -6,13 +6,13 @@
 //  Copyright © 2015 TRON. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "Register.h"
 
-@interface ViewController ()
+@interface Register ()
 
 @end
 
-@implementation ViewController
+@implementation Register
 
 - (void)viewDidLoad {
     [super viewDidLoad];
